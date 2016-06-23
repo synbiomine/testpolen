2bbfd0ba4c688b960d950ba4fad1c0af95b32cb6
Justin Clark-Casey <justincc@goose.sysbiol.private.cam.ac.uk>
2016-06-23 13:49:27 +0100

    Do hello world to polen

--- src/main/java/TestPolen.java
+++ src/main/java/TestPolen.java
@@ -1,5 +1,18 @@
+import java.util.Date;
+import java.util.List;
+import uk.ac.ncl.flowers.polen.*;
+
 public class TestPolen {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String serviceUri = "http://www.w3id.org/synbio/polen";
+        MessagingSystem messagingSystem = new MessagingSystem.Microbase(serviceUri);
+
+        try {
+            List messages = messagingSystem.poll("Part", new Date(2013, 12, 1), 2);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
